7e68889406e7c4f705d41d562c7257e89d7cf6ca
Bartolomeo <128607@hotmail.nl>
2025-12-03 20:59:16 +0100

    Initial commit

---
+++ AdventOfCode2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2025;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SecretEntrance secretEntrance = new SecretEntrance();
+        Console.WriteLine(secretEntrance.GetResultPartTwo());
+    }
+}
