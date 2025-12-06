92e5e574fea1ba04345f0f8a979d433a927bcdf0
Bartolomeo <128607@hotmail.nl>
2025-12-06 16:55:25 +0100

    Finished PrintingDepartment

--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -5,7 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
 
-        Lobby lobby = new Lobby();
-        Console.WriteLine(lobby.GetResult(12));
+        PrintingDepartment pd = new PrintingDepartment();
+        Console.WriteLine(pd.GetResultPartTwo());
     }
 }
