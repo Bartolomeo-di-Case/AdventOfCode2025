3439ac5059594620024726c9505085e827895e19
Bartolomeo <128607@hotmail.nl>
2025-12-07 14:20:37 +0100

    Finished Laboratories

--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        TrashCompactor trashCompactor = new TrashCompactor();
-        Console.WriteLine(trashCompactor.GetResultPartTwo());
+       Laboratories laboratories = new Laboratories();
+        Console.WriteLine(laboratories.GetResultPartTwo());
     }
 }
