199db011e82501b91954916a93727d7c2e932400
Bartolomeo <128607@hotmail.nl>
2025-12-08 20:35:51 +0100

    Finished Playground fuck fucking fuck fuck overflow

--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-       Laboratories laboratories = new Laboratories();
-        Console.WriteLine(laboratories.GetResultPartTwo());
+       Playground playground = new Playground(); 
+       Console.WriteLine(playground.GetResultPartTwo());
     }
 }
